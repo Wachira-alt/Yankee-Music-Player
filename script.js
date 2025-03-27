3065c907871a6f1b46e4481523a4c94594b133dc
Dennis Wachira <wachiradenis336@gmail.com>
2025-03-27 21:54:16 +0300

    feat: enable favorite songs load from local storage

--- script.js
+++ script.js
@@ -4,3 +4,7 @@ const shuffleBtn = document.getElementById("shuffle-btn");
 const playerContainer = document.createElement("div");
 playerContainer.id = "player-container";
 document.body.appendChild(playerContainer);
+// Load favorites from localStorage
+let favorites = JSON.parse(localStorage.getItem("favorites")) || [];
+favorites = favorites.filter(song => song && song.videoId && song.songTitle);
+localStorage.setItem("favorites", JSON.stringify(favorites));
