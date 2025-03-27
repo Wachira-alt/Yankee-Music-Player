6e2cdf747c2eef8226f9b092cac8ee3232e76341
Dennis Wachira <wachiradenis336@gmail.com>
2025-03-27 21:55:25 +0300

    feat: fetch trending music videos

--- script.js
+++ script.js
@@ -8,3 +8,13 @@ document.body.appendChild(playerContainer);
 let favorites = JSON.parse(localStorage.getItem("favorites")) || [];
 favorites = favorites.filter(song => song && song.videoId && song.songTitle);
 localStorage.setItem("favorites", JSON.stringify(favorites));
+// Function: Fetch Trending Music Videos
+async function fetchTopTracks() {
+  try {
+      const response = await fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=Top trending music&type=video&maxResults=5&key=${YOUTUBE_API_KEY}`);
+      const data = await response.json();
+      displayTracks(data.items, "playlist", "Popular");
+  } catch (error) {
+      console.error("Error fetching YouTube data:", error);
+  }
+}
