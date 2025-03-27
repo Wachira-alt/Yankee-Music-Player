71d288d100af9614de46db807a096c1d63a1d9da
Dennis Wachira <wachiradenis336@gmail.com>
2025-03-27 21:52:11 +0300

    feat: add global constants and variables

---
+++ script.js
@@ -0,0 +1,6 @@
+// Global Constants & Variables
+const YOUTUBE_API_KEY = "AIzaSyB_mmD9wqQbfr-73CgeBuFUtbKRMotCfQM";
+const shuffleBtn = document.getElementById("shuffle-btn");
+const playerContainer = document.createElement("div");
+playerContainer.id = "player-container";
+document.body.appendChild(playerContainer);
